0c420a1137e21fbc6a3c6a311b03d5db78a29d7f
Rodrigo Sesterheim <rodrigows09@gmail.com>
2024-07-27 19:19:58 -0300

    feat: dependencies

--- schedule/create.go
+++ schedule/create.go
@@ -1,19 +1,45 @@
 package schedule
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
+	createScheduleCmd.Flags().String("name", name, "Name of the schedule")
+	viper.BindPFlag("name", createScheduleCmd.Flags().Lookup("name"))
+
 	rootCmd.AddCommand(createScheduleCmd)
 }
 
-var createScheduleCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Create a new schedule",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Create a new schedule")
-	},
+var (
+	name string
+	date string
+	time string
+
+	createScheduleCmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create a new schedule",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := createSchedule(); err == "" {
+				fmt.Println("Name is required")
+			}
+
+			fmt.Printf("Schedule %s created\n", name)
+		},
+		Aliases: []string{"c", "-c"},
+	}
+)
+
+func createSchedule() string {
+	name = viper.GetString("name")
+
+	if name == "" {
+		return errors.New("name is required").Error()
+	}
+
+	return name
 }

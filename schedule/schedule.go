c9735d534f3e66be2f4cbc0f9f276994c3ed9789
Rodrigo Sesterheim <rodrigows09@gmail.com>
2024-07-27 17:58:44 -0300

    feat(schedule): execute cobra

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -2,12 +2,23 @@ package schedule
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
-func Hello(name string) string {
-	// use := to declare and assign a variable
-	// equals to ':=' = 'var message string'
-	message := fmt.Sprintf("Hello, %v!", name)
+var rootCmd = &cobra.Command{
+	Use:   "schedule",
+	Short: "schedule is a CLI tool to manage your schedule",
+	Long:  `schedule is a CLI tool to manage your schedule. You can add, list, update and delete your schedule`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to Schedule CLI")
+	},
+}
 
-	return message
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }

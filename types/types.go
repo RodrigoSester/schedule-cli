ecc96944103f97444428789a5b4ac1a21edaa6cb
Rodrigo Sesterheim <64436168+RodrigoSester@users.noreply.github.com>
2024-08-07 14:19:00 +0000

    feat: add frequency and type in struct

--- types/types.go
+++ types/types.go
@@ -1,9 +1,11 @@
 package types
 
 type Schedule struct {
-	Name string `yaml:"name"`
-	Date string `yaml:"date"`
-	Time string `yaml:"time"`
+	Name      string `yaml:"name"`
+	Date      string `yaml:"date"`
+	Time      string `yaml:"time"`
+	Type      string `yaml:"type"`
+	Frequency string `yaml:"frequency"`
 }
 
 type ScheduleList struct {
